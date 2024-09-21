1af812f6d587deb6c1c33e6dd7c7ee5679e8854d
forevpurity <auvv310103@gmail.com>
2024-09-21 10:49:23 +0700

    Add enum to gadget request and seller application

--- WebApi/Features/Categories/CreateCategory.cs
+++ WebApi/Features/Categories/CreateCategory.cs
@@ -1,86 +1,76 @@
-﻿//using FluentValidation;
-//using Microsoft.EntityFrameworkCore;
-//using WebApi.Common.Endpoints;
-//using WebApi.Common.Exceptions;
-//using WebApi.Common.Filters;
-//using WebApi.Data;
-//using WebApi.Data.Entities;
-//using WebApi.Features.Brands.Mapper;
-//using WebApi.Features.Brands.Models;
-//using WebApi.Services.Storage;
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Common.Endpoints;
+using WebApi.Common.Exceptions;
+using WebApi.Common.Filters;
+using WebApi.Data;
+using WebApi.Data.Entities;
+using WebApi.Features.Categories.Mappers;
+using WebApi.Features.Categories.Models;
 
-//namespace WebApi.Features.Categories;
+namespace WebApi.Features.Categories;
 
-//public class CreateCategory
-//{
-//    public class Request
-//    {
-//        public int? ParentId { get; set; }
-//        public string Name { get; set; } = default!;
-//    }
-//    public sealed class Validator : AbstractValidator<Request>
-//    {
-//        public Validator()
-//        {
-//            RuleFor(r => r.Name)
-//                .NotEmpty()
-//                .WithMessage("Tên không được để trống");
-//        }
-//    }
+public class CreateCategory
+{
+    public class Request
+    {
+        public int? ParentId { get; set; }
+        public string Name { get; set; } = default!;
+    }
+    public sealed class Validator : AbstractValidator<Request>
+    {
+        public Validator()
+        {
+            RuleFor(r => r.Name)
+                .NotEmpty()
+                .WithMessage("Tên không được để trống");
+        }
+    }
 
-//    public sealed class Endpoint : IEndpoint
-//    {
-//        public void MapEndpoint(IEndpointRouteBuilder app)
-//        {
-//            app.MapPost("categories", Handler)
-//                .WithTags("Categories")
-//                .WithDescription("This API is to create a category")
-//                .WithSummary("Create Category")
-//                .Produces<BrandResponse>(StatusCodes.Status201Created)
-//                .WithJwtValidation()
-//                .WithRequestValidation<Request>()
-//                .WithRolesValidation(Role.Admin);
-//        }
-//    }
+    public sealed class Endpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost("categories", Handler)
+                .WithTags("Categories")
+                .WithDescription("This API is to create a category")
+                .WithSummary("Create Category")
+                .Produces<CategoryResponse>(StatusCodes.Status201Created)
+                .WithJwtValidation()
+                .WithRequestValidation<Request>()
+                .WithRolesValidation(Role.Admin);
+        }
+    }
 
-//    public static async Task<IResult> Handler(Request request, AppDbContext context, GoogleStorageService storageService)
-//    {
-//        if (await context.Brands.AnyAsync(b => b.Name == request.Name))
-//        {
-//            throw TechGadgetException.NewBuilder()
-//                .WithCode(TechGadgetErrorCode.WEB_01)
-//                .AddReason("name", "Tên thương hiệu đã tồn tại")
-//                .Build();
-//        }
+    public static async Task<IResult> Handler(Request request, AppDbContext context)
+    {
+        if (request.ParentId is not null
+            && !await context.Categories.AnyAsync(c => c.Id == request.ParentId))
+        {
+            throw TechGadgetException.NewBuilder()
+                .WithCode(TechGadgetErrorCode.WEB_00)
+                .AddReason("category", "Không tìm thấy thể loại")
+                .Build();
+        }
 
-//        string? logoUrl = null;
-//        try
-//        {
-//            logoUrl = await storageService.UploadFileToCloudStorage(request.Logo, Guid.NewGuid().ToString());
-//        }
-//        catch (Exception)
-//        {
-//            if (logoUrl != null)
-//            {
-//                await storageService.DeleteFileFromCloudStorage(logoUrl);
-//            }
-//            throw TechGadgetException.NewBuilder()
-//                .WithCode(TechGadgetErrorCode.WES_00)
-//                .AddReason("logo", "Lỗi khi lưu logo")
-//                .Build();
-//        }
+        if (await context.Categories.AnyAsync(c => c.Name == request.Name))
+        {
+            throw TechGadgetException.NewBuilder()
+                .WithCode(TechGadgetErrorCode.WEB_01)
+                .AddReason("category", "Thể loại đã tồn tại")
+                .Build();
+        }
 
-//        var brand = new Brand
-//        {
-//            Name = request.Name,
-//            LogoUrl = logoUrl
-//        };
+        var category = new Category
+        {
+            Name = request.Name,
+            ParentId = request.ParentId,
+            IsAdminCreated = true
+        };
 
-//        context.Brands.Add(brand);
-//        await context.SaveChangesAsync();
+        context.Categories.Add(category);
+        await context.SaveChangesAsync();
 
-//        var response = brand.ToBrandResponse();
-
-//        return Results.Json(response, statusCode: 201);
-//    }
-//}
+        return Results.Json(category.ToCategoryResponse(), statusCode: StatusCodes.Status201Created);
+    }
+}
