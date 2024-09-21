bb58d0ae0eee87180aa5cb7798234c0a2fe7c38d
forevpurity <auvv310103@gmail.com>
2024-09-21 12:08:24 +0700

    .

---
+++ WebApi/Features/Categories/GetCategoryById.cs
@@ -0,0 +1,5 @@
+﻿namespace WebApi.Features.Categories;
+
+public class GetCategoryById
+{
+}
---
+++ WebApi/Features/Categories/Models/CategoryDetailResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.Features.Categories.Models;
+
+public class CategoryDetailResponse
+{
+    public int Id { get; set; }
+    public ICollection<CategoryResponse> Parent { get; set; } = [];
+    public string Name { get; set; } = default!;
+    public bool IsAdminCreated { get; set; }
+}
